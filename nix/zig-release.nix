bb88ae14f1a87b094e85a77d6477f1bca2263afc
mo <mocompute@outlook.com>
2024-10-07 21:38:32 +1300

    add nix build files

---
+++ nix/zig-release.nix
@@ -0,0 +1,11 @@
+{
+  version = "0.14.0-dev.1765+73de620ad";
+  src = {
+    rev = "73de620ad50121bacb0b9b376efd50b5444485b8";
+    hash = "sha256-dpzYfJrUBnYsYeFCZsE7O+xgD6LHj1cl526lwqIPrf4=";
+  };
+  binaries = {
+    x86_64-linux.sha256 = "94784ebd911b6540f49b5c2d34a09ec2e6732561d9f45f915fc38e5b6049acf3";
+    aarch64-linux.sha256 = "453c8ae72ed6b48be0e545f2dcba629fdfd80fe7d1580bbc2ea7c2bbca19280e";
+  };
+}
